c134105c53cb2c38d1372b43c814e57a7f3a670b
RomGPT <rumblade@gmail.com>
2026-06-09 14:41:13 +0700

    Add GridBot Python package scaffold

---
+++ src/afree_gridbot/__init__.py
@@ -0,0 +1,11 @@
+"""AFree GridBot research helpers."""
+
+from .grid import GridConfig, build_grid_levels
+from .backtest import BacktestResult, run_simple_grid_backtest
+
+__all__ = [
+    "BacktestResult",
+    "GridConfig",
+    "build_grid_levels",
+    "run_simple_grid_backtest",
+]
